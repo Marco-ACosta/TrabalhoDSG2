d35d6e9b8e0e3b12b48c84dba90aae126c0b046a
lazaro lorenzi <lazarolorenzi123@gmail.com>
2024-05-24 01:09:40 -0300

    att

--- src/context/AuthContext.ts
+++ src/context/AuthContext.ts
@@ -1,7 +1,7 @@
 "use client";
 
 import { createContext, useContext } from "react";
-import { User } from "../services/firebase/";
+import { User} from "firebase/auth";
 
 interface AuthContextType {
     userAuth: User | null;
